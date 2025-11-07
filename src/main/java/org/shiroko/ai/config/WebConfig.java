95e93e791457eb26bc11e75ffaf33327075453ce
KurimulaAiri <1095370199@qq.com>
2025-11-07 09:45:52 +0800

    跨域修复8

--- src/main/java/org/shiroko/ai/config/WebConfig.java
+++ src/main/java/org/shiroko/ai/config/WebConfig.java
@@ -47,8 +47,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 对所有接口生效（/**表示匹配所有路径）
-                .allowedOrigins("http://localhost:8081/", "http://localhost:25502/") // 允许的源（多个用逗号分隔）
-                .allowedOriginPatterns("https://*.kuaidimao.com/", "https://*.wyhlife.com/", "http://localhost:25502/") // 允许的源（多个用逗号分隔，支持通配符）
+                .allowedOrigins("http://localhost:8081", "http://localhost:25502") // 允许的源（多个用逗号分隔）
+                .allowedOriginPatterns("https://*.kuaidimao.com", "https://*.wyhlife.com", "http://localhost:25502") // 允许的源（多个用逗号分隔，支持通配符）
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法（包括预检请求OPTIONS）
                 .allowedHeaders("Authorization", "Content-Type") // 允许的请求头
                 .allowCredentials(true) // 允许携带Cookie
